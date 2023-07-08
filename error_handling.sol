79c8e3eb21dc54f5591829d5d3129e78615cde1c
Bhupati Parmar <87934381+Kennys13@users.noreply.github.com>
2023-07-08 09:37:05 +0530

    Update error_handling.sol

--- error_handling.sol
+++ error_handling.sol
@@ -1,19 +1,20 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract ErrorHandlingContract {
-    function requireExample(uint256 a, uint256 b) external pure returns (uint256) {
+contract Error_handling {
+    function require_test(uint256 a, uint256 b) external pure returns (uint256) {
         require(a > b, "a must be greater than b");
         uint256 result = a - b;
         return result;
     }
     
-    function assertExample(uint256 a, uint256 b) external pure returns (uint256) {
+    function assert_test(uint256 a, uint256 b) external pure returns (uint256) {
         assert(a > b);
         uint256 result = a - b;
         return result;
     }
     
-    function revertExample(uint256 a, uint256 b) external pure returns (uint256) {
+    function revert_test(uint256 a, uint256 b) external pure returns (uint256) {
         if (a <= b) {
             revert("a must be greater than b");
         }
